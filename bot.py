091e39356a9f4ee240dfc4bdb319fb6d63bff7c1
cz303 <56400025+cz303@users.noreply.github.com>
2019-10-24 20:39:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,6 +55,7 @@ def twitter(update, context):
         user = str(update.message.chat.username)
         twtr_user = data['twitter'][user]
         twitter_msg = "Follow dSTAR twitter page, make some like, comment, share and you will receive 5 TRX(TRON). https://twitter.com/dSTARLab."
+        reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
         update.message.reply_text(twitter_msg,reply_markup=reply_markup)
         msg = 'Your twitter username is {}'.format(twtr_user)
         reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)

047d165e375f27e3937702002f42cf4336e1147c
cz303 <56400025+cz303@users.noreply.github.com>
2019-10-25 00:31:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ signup = config['signup']
 refr = config['ref']
 admins = config['admins']
 data = []
-dash_key = [['🐦 Twitter','🚀 dSTAR','💸TRX'],['💎 Referral Link','🤝Referred'],['💼 Balance','📍Details']]
+dash_key = [['Twitter','dSTAR','TRX'],['Referral Link','Referred'],['Balance','Details']]
 admin_key = [['Users','Get List']]
 
 webhook_url = 'Your Webook'

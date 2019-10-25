53f1a199c07381d43026f6e1b5ed7ab44a4dfdfe
cz303 <56400025+cz303@users.noreply.github.com>
2019-10-25 19:44:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ def start(update, context):
             data['id'][user] = data['total']
             data['process'][user] = "twitter"
         else:
-            welcome_msg = "<b>Welcome to dSTAR airdrop bot!</b>\n\n"+"Follow dSTAR telegram channel. https://t.me/dstarlab.\n\n"+"Download dSTAR messenger and you will receive 15 TRX(TRON).\n\n"+"Follow dSTAR twitter page, make some like, comment, share and you will receive 5 TRX(TRON).\n\n"+"Additionally, you can receive 5 TRX(TRON) for each invited user!"
+            welcome_msg = "<b>Welcome to dSTAR airdrop bot!</b>\n\n"+"Follow dSTAR telegram channel. https://t.me/dstarlab.\n\n"+"Download dSTAR messenger and you will receive 15 TRX(TRON).\n\n"+"Follow dSTAR twitter page, make some like, comment, share and you will receive 5 TRX(TRON).\n\n"+"Additionally, you can receive 5 TRX(TRON) for each invited user!\n\n"+"https://dstarlab.com"
             reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
             update.message.reply_text(welcome_msg,reply_markup=reply_markup,parse_mode= "HTML")
 
@@ -173,7 +173,7 @@ def bal(update, context):
         update.message.reply_text(msg,reply_markup=reply_markup)
 
 def detail(update, context):
-            welcome_msg = "<b>Welcome to dSTAR airdrop bot!</b>\n\n"+"Follow dSTAR telegram channel. https://t.me/dstarlab\n\n"+"Download dSTAR messenger and you will receive 15 TRX(TRON).\n\n"+"Follow dSTAR twitter page, make some like, comment, share and you will receive 5 TRX(TRON).\n\n"+"Additionally, you can receive 5 TRX(TRON) for each invited user!"
+            welcome_msg = "<b>Welcome to dSTAR airdrop bot!</b>\n\n"+"Follow dSTAR telegram channel. https://t.me/dstarlab\n\n"+"Download dSTAR messenger and you will receive 15 TRX(TRON).\n\n"+"Follow dSTAR twitter page, make some like, comment, share and you will receive 5 TRX(TRON).\n\n"+"Additionally, you can receive 5 TRX(TRON) for each invited user!\n\n"+"https://dstarlab.com"
             reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
             update.message.reply_text(welcome_msg,reply_markup=reply_markup,parse_mode= "HTML")
 
